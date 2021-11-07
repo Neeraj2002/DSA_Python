a895c0929ef6872bba88f86dcbe2c6870f1e3424
Neeraj Rathod <75560703+Neeraj2002@users.noreply.github.com>
2021-11-07 20:38:41 +0530

    Learning DSA

---
+++ CPP/ARRAYS-STRINGS/Array_first-repeating-element.cpp
@@ -0,0 +1,13 @@
+// Given an array arr[] of size n. The task is to find the first repearting element in the 
+// array of integers, i.e., an element that occurs more than once and whose index of first 
+// occurence is the smallest.
+#include<iostream>
+using namespace std;
+// LOGIC:
+// 1. Check if the element is repeating, we maintain an arr in idx[], which stores the first
+// occurence of a particular element a[i].
+//  i.  Initialise the idx[] with -1, and miniidx with INT_MAX.
+//  ii. Keep updating idx[], while traversing the given array.
+int main(){
+
+}
