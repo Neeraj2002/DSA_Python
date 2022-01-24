b214a86766cadcbd64900d9cd5f7b7bc21f86508
Neeraj Rathod <75560703+Neeraj2002@users.noreply.github.com>
2022-01-24 22:25:41 +0530

    Learning DSA

---
+++ CPP/SORTING_ALGO/pair.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    pair <int, char> p; // Within the <> any datatypes can be used
+    // Accessing the pair
+    p.first = 3;
+    p.second = "F";
+    
+
+
+    return 0;
+}
