8748d209378ba046e149bb75b1e7957c603e77d1
Neeraj Rathod <75560703+Neeraj2002@users.noreply.github.com>
2022-02-10 15:56:50 +0530

    Learning DSA

---
+++ CPP/SORTING_ALGO/vector_pair_question.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool myCompare(pair<int,int> p1, pair<int,int> p2){
+    return p1.first<p2.first;
+}
+
+// Reducing the array to its minimum size by reducing the values
+
+// array   :   10 16 7  14 5  3  12 9
+// index   :   0  1  2  3  4  5  6  7
+
+// redarray:   4  7  2  6  1  0  5  3  
+// redindex:   0  1  2  3  4  5  6  7
+
+
+int main(){
+    int arr[] = {10, 16, 7, 14, 5, 3, 12, 9};
+    vector <pair <int, int> > v;
+    for (int i = 0; i < (sizeof(arr)/sizeof(arr[0])); i++)
+    {
+        v.push_back(make_pair(arr[i], i));
+    }
+    sort(v.begin(), v.end(), myCompare);
+    for (int i = 0; i < v.size(); i++)
+    {
+        arr[v[i].second] = i;
+    }
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout<< arr[i]<<" "<< endl;
+    }
+    
+    return 0;
+}
